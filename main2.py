0951ebe212f206c794fb6bee9836f9ef559102d8
SabigBenmumin <totpophy14755@gmail.com>
2022-06-02 21:44:47 +0700

    part1 update not complete

--- main2.py
+++ main2.py
@@ -5,30 +5,14 @@ from os.path import join, basename
 from collections import deque
 from lane_detection import color_frame_pipeline
 
-if __name__ == '__main__':
-
-    # test on videos
-    test_videos = 'solidWhiteRight.mp4'
-
-    for test_video in test_videos:
-
-        cap = cv2.VideoCapture(test_video)
-        out = cv2.VideoWriter(join('out', 'videos', basename(test_video)),
-                              fourcc=cv2.VideoWriter_fourcc(*'DIVX'),
-                              fps=20.0, frameSize=(resize_w, resize_h))
-
-        frame_buffer = deque(maxlen=10)
-        while cap.isOpened():
-            ret, color_frame = cap.read()
-            if ret:
-                color_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
-                color_frame = cv2.resize(color_frame, (resize_w, resize_h))
-                frame_buffer.append(color_frame)
-                blend_frame = color_frame_pipeline(frames=frame_buffer, solid_lines=True, temporal_smoothing=True)
-                out.write(cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR))
-                cv2.imshow('blend', cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR)), cv2.waitKey(1)
-            else:
-                break
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+ori_img = cv2.imread('solidWhiteRight.jpg')
+cv2.imshow('original img',ori_img)
+cv2.waitKey(0)
+bgr_img = cv2.cvtColor(ori_img,cv2.COLOR_BGR2RGB)
+cv2.imshow('test rgb2bgr',bgr_img)
+cv2.waitKey(0)
+out_image = color_frame_pipeline([bgr_img], solid_lines=True)
+#cv2.imshow(cv2.cvtColor(out_image,cv2.COLOR_RGB2BGR))
+plt.imshow(out_image)
+plt.waitforbuttonpress()
+cv2.waitKey(0)
