113f4efdc1ec6c4fb21cf6d2ac40fdee4c52f4d7
SabigBenmumin <totpophy14755@gmail.com>
2022-06-22 23:49:38 +0700

    canny

--- model_1/basic lane finding/find_lanes.py
+++ model_1/basic lane finding/find_lanes.py
@@ -1,10 +1,10 @@
 import cv2
+from cv2 import blur
 import numpy as np
 
 ori_img = cv2.imread('test_images\solidWhiteRight.jpg')
 gray_img = cv2.cvtColor(ori_img,cv2.COLOR_RGB2GRAY)
 blur_img = cv2.GaussianBlur(gray_img,(5,5),0)
-cv2.imshow('gray scale image',gray_img)
-cv2.waitKey(0)
-cv2.imshow('blur',blur_img)
+canny = cv2.Canny(blur_img,50,150)
+cv2.imshow('canny',canny)
 cv2.waitKey(0)
